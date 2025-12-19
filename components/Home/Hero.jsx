12bd0bc8366fb26b27831c4f1d6f291b200d6858
Régis Daum <contact@graphandco.com>
2025-12-20 00:06:57 +0100

    Styled hero

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -1,11 +1,34 @@
 "use client";
+
 import Copy from "@/components/Copy/Copy";
 import Button from "@/components/Button/Button";
 import { isInitialLoad } from "@/components/Preloader/Preloader";
-import { useResponsive } from "@/hooks/UseResponsive";
-import Image from "next/image";
 
-export default function Hero() {
+import { useRef } from "react";
+// import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+// import MagnetButton from "../ui/MagnetButton";
+import { useResponsive } from "@/hooks/UseResponsive";
+
+const Hero = () => {
+   const sectionRef = useRef(null);
+   const titleRef = useRef(null);
+
+   //    const { scrollYProgress } = useScroll({
+   //       target: sectionRef,
+   //       offset: ["start start", "end start"],
+   //    });
+
+   //    const yTitle = useTransform(scrollYProgress, [0, 1], [0, -80]);
+   //    const yDesc = useTransform(scrollYProgress, [0, 1], [0, -120]);
+   //    const scaleBg = useTransform(scrollYProgress, [0, 1], [1, 1.3]);
+
+   //    const scrollToSection = () => {
+   //       const section = document.getElementById("notre-expertise");
+   //       if (section) {
+   //          section.scrollIntoView({ behavior: "smooth" });
+   //       }
+   //    };
    const { isPhone, isMobile, isTablet, isDesktop } = useResponsive();
    function getHeroImageSource() {
       if (isPhone) return "/hero/bg-hero-phone.avif";
@@ -14,57 +37,76 @@ export default function Hero() {
       if (isDesktop) return "/hero/bg-hero-desktop.avif";
       return "/hero/bg-hero-desktop.avif";
    }
+
    const heroImageSource = getHeroImageSource();
 
    return (
-      <section className="relative w-full h-[100svh] overflow-hidden">
-         <div className="absolute inset-0 z-0">
-            <Image
-               src={heroImageSource}
-               alt="Hero background"
-               fill
-               className="object-cover"
-               priority
-               sizes="100vw"
-            />
+      <section
+         id="hero"
+         ref={sectionRef}
+         className="relative min-h-screen overflow-hidden"
+      >
+         {/* Fond avec scale */}
+         <div className="absolute inset-0">
+            <div className="relative w-full h-full will-change-transform after:content-[''] after:absolute after:top-0 after:left-0 after:h-1/4 after:w-full after:bg-linear-to-t after:from-transparent after:to-black/70">
+               <Image
+                  src={heroImageSource}
+                  alt="Hero"
+                  fill
+                  className="object-cover"
+                  priority
+                  sizes="100vw"
+               />
+            </div>
          </div>
-         <div className="container flex justify-center items-end relative z-10">
-            <div className="w-[65%] h-full flex flex-col justify-between items-center text-center max-[1000px]:w-full max-[1000px]:justify-center max-[1000px]:gap-8">
-               <div className="hero-header">
-                  <Copy animateOnScroll={false} delay={0.75}>
-                     <div className="text-10xl font-bold">Graph and Co</div>
-                  </Copy>
-               </div>
 
-               <div className="absolute bottom-0 left-0 w-full p-8">
+         <div className="absolute inset-0 flex flex-col justify-center px-5">
+            {/* <div className="mb-8 flex flex-col items-end gap-1 text-white font-semibold">
+               <div>Interface utilisateur</div>
+               <div>Expérience utilisateur</div>
+               <div>Sécurité</div>
+               <div>Accessibilité</div>
+            </div> */}
+
+            <div ref={titleRef}>
+               <div className="text-10xl title-font text-white font-semibold leading-[0.8] xs:mix-blend-exclusion">
                   <Copy
                      animateOnScroll={false}
-                     delay={isInitialLoad ? 6.35 : 1.65}
+                     //  delay={isInitialLoad ? 6.35 : 1.65}
+                     delay={1.65}
                   >
-                     <div className="flex justify-between items-end">
-                        <p className="sm">&copy; Codegrid Dept.</p>
-                        <p className="sm">( Workroom 101 )</p>
-                     </div>
+                     <span>Graph & Co</span>
                   </Copy>
-               </div>
-
-               <div className="w-[50%] flex flex-col items-center gap-8 max-[1000px]:w-full max-[1000px]:gap-8">
-                  <Copy
-                     animateOnScroll={false}
-                     delay={isInitialLoad ? 6.05 : 1.15}
-                  >
-                     <p className="lg">
-                        We build visuals, stories, and systems for people who
-                        like their creativity a little unpredictable.
-                     </p>
-                  </Copy>
-
-                  <Button delay={isInitialLoad ? 6.35 : 1.55} href="/studio">
-                     Visit the Studio
-                  </Button>
                </div>
             </div>
+
+            <div className="relative mt-8 space-y-4">
+               <h1 className="text-2xl title-font text-white font-medium leading-[0.9] xs:mix-blend-exclusion">
+                  <Copy animateOnScroll={false} delay={1.65}>
+                     <span>
+                        Votre agence web à Colmar - Création de sites internet
+                        sur-mesure{" "}
+                     </span>
+                  </Copy>
+               </h1>
+               <Copy
+                  animateOnScroll={false}
+                  delay={1.65}
+                  className="text-white max-w-md"
+               >
+                  <span>
+                     Nous sommes spécialisés dans la réalisation de sites web.
+                     Moderne et intuitif, votre site sera un puissant levier
+                     pour accroitre la vitalité de votre entreprise.
+                  </span>
+               </Copy>
+            </div>
+            {/* <div className="relative mt-12 mx-auto">
+               <MagnetButton onClick={scrollToSection} />
+            </div> */}
          </div>
       </section>
    );
-}
+};
+
+export default Hero;
